37889ab61692baf9aa5ec3ff8009267b53e3d40d
euics <pjh575157@gmail.com>
2024-04-02 18:46:01 +0900

    환율 정보 Open API를 활용한 간단한 배치 만들기

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -2,12 +2,14 @@ package com.example.springbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBatchApplication.class, args);
+        ConfigurableApplicationContext context
+                = SpringApplication.run(SpringBatchApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/example/springbatch/batch/ExchangeBatchConfig.java
@@ -0,0 +1,52 @@
+package com.example.springbatch.batch;
+
+import com.example.springbatch.dto.ExchangeDto;
+import com.example.springbatch.util.ExchangeUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class ExchangeBatchConfig {
+    private final ExchangeUtils exchangeUtils;
+
+    @Bean
+    public Job exchangeJob(JobRepository jobRepository,
+                           Step step) {
+        return new JobBuilder("exchangeJob", jobRepository)
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step step(JobRepository jobRepository, PlatformTransactionManager tm, Tasklet tasklet) {
+        return new StepBuilder("step", jobRepository)
+                .tasklet(tasklet, tm)
+                .build();
+    }
+
+    @Bean
+    public Tasklet tasklet() {
+        return ((contribution, chunkContext) -> {
+            List<ExchangeDto> exchangeDtoList = exchangeUtils.getExchangeDataAsDtoList();
+
+            for (ExchangeDto exchangeDto : exchangeDtoList) {
+                System.out.println("통화 : " + exchangeDto.getCur_nm());
+                System.out.println("환율 : " + exchangeDto.getDeal_bas_r());
+                // 추가적인 필드가 있다면 출력 또는 활용
+            }
+            return RepeatStatus.FINISHED;
+        });
+    }
+}
